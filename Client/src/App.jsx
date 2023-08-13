63e831131a8ea737cb89506b2037be985f0a160d
Aditya Narayan Jaiswal <nlaxmi886@gmail.com>
2023-08-13 19:41:24 +0530

    Added basic functionality to dashboard and homepage>

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,22 @@
-// import React from "react"
+import React, { Profiler } from "react"
 import { Routes, Route, BrowserRouter } from "react-router-dom"
-// import Login from "./Login"
+import Login from "./Login"
 import Dashboard from "./Dashboard"
+import Employee from "./Employee"
+import Home from "./Home"
+import Profile from "./Profile"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Dashboard/>} /></Routes>
-      {/* <Route path='login' element={<Login/>}></Route> */}
+      <Route path="/" element={<Dashboard/>}>
+        <Route path='' element={<Home/>}></Route>
+        <Route path='/employee' element={<Employee/>}></Route>
+        <Route path='profile' element={<Profile/>}></Route>
+      </Route>
+      <Route path='login' element={<Login/>}></Route>
+    </Routes>
     </BrowserRouter>
   )
 }
